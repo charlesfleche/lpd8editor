5c1a98899b8a29bc52b543fc0c13ab4d4f044872
Charles Flèche <charles.fleche@free.fr>
2018-03-09 21:16:17 +0100

    Subclass QSqlTableModel directly for ProgramsModel

--- src/programsmodel.h
+++ src/programsmodel.h
@@ -1,27 +1,16 @@
 #ifndef PROGRAMSMODEL_H
 #define PROGRAMSMODEL_H
 
-#include <QAbstractItemModel>
+#include <QSqlTableModel>
 
 class QSqlTableModel;
 
-class ProgramsModel : public QAbstractItemModel
+class ProgramsModel : public QSqlTableModel
 {
     Q_OBJECT
 
 public:
     ProgramsModel(QObject* parent = Q_NULLPTR);
-
-    int columnCount(const QModelIndex &parent) const Q_DECL_OVERRIDE;
-    QVariant data(const QModelIndex &index, int role) const Q_DECL_OVERRIDE;
-    Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const Q_DECL_OVERRIDE;
-    QModelIndex index(int row, int column, const QModelIndex &parent) const Q_DECL_OVERRIDE;
-    QModelIndex parent(const QModelIndex &child) const Q_DECL_OVERRIDE;
-    int rowCount(const QModelIndex &parent) const Q_DECL_OVERRIDE;
-
-private:
-    QSqlTableModel* m_programs;
 };
 
 #endif // PROGRAMSMODEL_H
