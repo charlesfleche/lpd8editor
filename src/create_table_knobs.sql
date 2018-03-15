8347e50a5cc2b464c33fdddf02e193619e8efdd9
Charles Flèche <charles.fleche@free.fr>
2018-03-15 22:13:26 +0100

    Fix constraints

--- src/create_table_knobs.sql
+++ src/create_table_knobs.sql
@@ -1,5 +1,5 @@
 create table knobs(programId integer NOT NULL,
-                   controlId integer NOT NULL check (controlId >= 1 and controldId <= 8),
+                   controlId integer NOT NULL check (controlId >= 1 and controlId <= 8),
                    cc integer NOT NULL check (cc >= 0 and cc <= 127),
                    low integer NOT NULL check (low >= 0 and low <= 127),
                    high integer NOT NULL check (high >= 0 and high <= 127),
--- src/create_table_pads.sql
+++ src/create_table_pads.sql
@@ -1,5 +1,5 @@
 create table pads(programId integer NOT NULL,
-                  controlId integer NOT NULL check (controlId >= 1 and controlId <= 8,
+                  controlId integer NOT NULL check (controlId >= 1 and controlId <= 8),
                   note integer NOT NULL check (note >= 0 and note <= 127),
                   pc integer NOT NULL check (pc >= 0 and pc <= 127),
                   cc integer NOT NULL check (cc >= 0 and cc <= 127),
